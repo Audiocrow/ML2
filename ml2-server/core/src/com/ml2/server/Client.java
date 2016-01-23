4d6bc65f1cab1bd6508f8326a2746a89f4c38c5e
Audiocrow <mrcrowk@gmail.com>
2016-01-23 15:50:48 -0800

    Basic server code started

---
+++ ml2-server/core/src/com/ml2/server/Client.java
@@ -0,0 +1,47 @@
+package com.ml2.server;
+
+import java.io.InputStream;
+
+import com.badlogic.gdx.net.Socket;
+import com.badlogic.gdx.utils.Disposable;
+
+public class Client implements Disposable {
+	public final String TAG = Client.class.getName();
+	
+	private final Socket socket;
+	public final String address;
+	
+	Client(Socket s) {
+		socket = s;
+		address = s.getRemoteAddress();
+	}
+	
+	public InputStream getInputStream() { return socket.getInputStream(); }
+	@Override
+	public int hashCode() {
+		/*RECALL: Whenever it is invoked on the same object more than once during an execution of a Java application,
+		 * the hashCode method must consistently return the same integer, provided that 
+		 * no information used in equals comparisons on the object is modified.
+		 * This integer need not remain consistent from one execution of an application to another execution of the same application.
+		 * 
+		 * TLDR; ONLY USE VALUES THAT WON'T CHANGE TO COMPUTE THE HASHCODE
+		 */
+		int hash = 73;
+		hash = 73*hash + address.hashCode();
+		return hash;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		/*Note: Client will equal another client even if the sockets are different
+		 * which shouldn't matter since the IP Addresses are the only important difference
+		 */
+		if(obj == null) return false;
+		if(obj == this) return true;
+		if(!(obj instanceof Client)) return false;
+		Client other = (Client)obj;
+		if(!address.equals(other.address)) return false;
+		return true;
+	}
+	@Override
+	public void dispose() { socket.dispose(); }
+}
